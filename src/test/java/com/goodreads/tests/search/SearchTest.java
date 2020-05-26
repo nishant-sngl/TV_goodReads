fa041b5313cc91ec29f3c53ec50412cb0ef03715
nishantsinghal <nishant@getvymo.com>
2020-05-26 15:52:08 +0530

    closing driver

--- src/test/java/com/goodreads/tests/search/SearchTest.java
+++ src/test/java/com/goodreads/tests/search/SearchTest.java
@@ -3,6 +3,7 @@ package com.goodreads.tests.search;
 import com.goodreads.base.Base;
 import com.goodreads.pages.HomePage;
 import com.goodreads.pages.SearchPage;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -51,5 +52,10 @@ public class SearchTest extends Base {
             System.out.println("As per the given inputs, i guess the book name is: " + val.getKey());
             break;
         }
+    }
+
+    @AfterMethod
+    private void closeTest(){
+        driver.quit();
     }
 }
